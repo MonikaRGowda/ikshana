a1beac6eecacbd7bef04aebd0a31f5ac0448f6f6
MonikaRGowda <monikargowda7@gmail.com>
2026-08-30 01:25:47 +0530

    Update Ikshana homepage

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -2,6 +2,6 @@ import { createFileRoute, redirect } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/")({
   beforeLoad: () => {
-    throw redirect({ to: "/booth" });
+    throw redirect({ to: "/login" });
   },
 });
